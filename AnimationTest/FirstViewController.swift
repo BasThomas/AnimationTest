21d9f51dd1f200d57bd7e7393f5b9f32d56d2ece
Bas Broek <bas@basbroek.nl>
2015-10-01 08:45:21 +0200

    Initial project

---
+++ AnimationTest/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AnimationTest
+//
+//  Created by Bas Broek on 01/10/15.
+//  Copyright © 2015 iCulture. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+}
---
+++ AnimationTest/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AnimationTest
+//
+//  Created by Bas Broek on 01/10/15.
+//  Copyright © 2015 iCulture. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+}
